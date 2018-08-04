e1d32d9d143be4659ccdc186a082a5d01df65ed0
skchauhan <skrajput42@gmail.com>
2018-08-04 17:24:36 +0530

    Add files via upload

---
+++ laravel_permission/admin-view/sidebar.blade.php
@@ -0,0 +1,32 @@
+<div class="col-md-3">
+    <div class="card">
+        <div class="card-header">
+            Sidebar
+        </div>
+
+        <div class="card-body">
+            <ul class="nav" role="tablist">
+                <li role="presentation">
+                    <a href="{{ url('/admin') }}">
+                        Dashboard
+                    </a>
+                </li>
+                <li role="presentation">
+                    <a href="{{ url('/admin/role') }}">
+                        Role
+                    </a>
+                </li>
+                <li role="presentation">
+                    <a href="{{ url('/admin/permission') }}">
+                        Permission
+                    </a>
+                </li>
+                <li role="presentation">
+                    <a href="{{ url('/admin/user') }}">
+                        User
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
